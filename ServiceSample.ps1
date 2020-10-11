eb39f905eb5d1bb890663cd7108b549e84a7915d
Jason Murata <jason.murata@gmail.com>
2020-10-11 00:52:00 +0000

    Added Service Resource
    Added Service Resource

---
+++ ServiceSample.ps1
@@ -0,0 +1,17 @@
+﻿Configuration ServiceSample {
+
+    # Import the module that contains the File resource.
+    Import-DscResource -ModuleName PsDesiredStateConfiguration
+    Import-DscResource -Name Service
+
+    # The Node statement specifies which targets to compile MOF files for, when
+    # this configuration is executed.
+    Node 'localhost' {
+
+        # The Service Resource 
+        Service "Spooler:Running" {
+            Name = "Spooler"
+            State = "Running"
+        }
+    }
+}
